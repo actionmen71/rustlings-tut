2ceabe8cc80c2e4f81f6addad3740a3bb1041488
KS <actionmen71@gmail.com>
2024-10-22 13:15:23 +0530

    xander

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -1,4 +1,8 @@
 fn main() {
     // TODO: Fix the code to print "Hello world!".
-    printline!("Hello world!");
+    println!("Hello world!");
+    let regular_string = "This is a \"normal\" string with an escaped newline\n";
+    // let raw_string = r"This is a raw string with no need to escape \"special\" characters or newlines\n";
+    println!("{}",regular_string);
+
 }
