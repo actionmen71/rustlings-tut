b7827ce57fce48cabed3a079c1ed863a7c7cf415
KS <actionmen71@gmail.com>
2024-11-24 11:05:25 +0530

    xander commmit

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -2,10 +2,15 @@
 fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
-    } else {
-        1
+    } 
+    else if fizzish == "fuzz" {
+        "bar"}
+    else {
+        "baz"
     }
 }
+
+ 
 
 fn main() {
     // You can optionally experiment here.
